52fccd5a5eeff0db3d416b77b1e25b6d1e528caf
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-09-05 08:31:25 +0200

    Added PrintCheeps Method
    Added method to abstract UserInterface interactions away from Program.cs

--- UserInterface.cs
+++ UserInterface.cs
@@ -2,5 +2,11 @@
 
 public static class UserInterface
 {
-    
+    public static void PrintCheeps(IEnumerable<Cheep> cheeps)
+        {
+            foreach (var vCheep in cheeps)
+            {
+                Console.WriteLine(vCheep);
+            }
+        }
 }
