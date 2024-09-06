fb21d96c46bad2a74a26e3d02a8895c406ab10e0
anthon <acah@itu.dk>
2024-09-06 12:22:59 +0200

    Brute-force implementation of csv-writer
    So we can start work on refactor of csv parsing.
    
    Co-authored-by: Mathias <mlao@itu.dk>
    Co-authored-by: Christoffer <gryn@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
 using System.Text.RegularExpressions;
 using Chirp.CLI;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 if (args.Length == 0)
 {
@@ -40,13 +44,22 @@ void Read()
 
 void WriteCheep(string message)
 {
+    //TODO: Fix writer to not override existing lines 
     string userName = Environment.UserName;
-    string currentTime = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
-    string outputMessage = $"{userName},\"{message}\",{currentTime}";
+    DateTimeOffset currentTime = DateTimeOffset.Now.DateTime;
+    var records = new List<Cheep>
+    {
+        new (userName, message, currentTime)
+    };
     
-    StreamWriter writer = new StreamWriter("chirp_cli_db.csv", true);
-    writer.WriteLine(outputMessage);
-    writer.Close();
+    using (var writer = new StreamWriter("chirp_cli_db.csv"))
+    using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+    {
+        csv.Context.RegisterClassMap<CheepMap>();
+        csv.WriteRecords(records);
+        csv.NextRecord();
+        writer.Flush();
+    }
 }
 
 List<string> ReadFile()
@@ -72,11 +85,11 @@ List<string> ReadFile()
 
 }
 
-public record Cheep
+public record Cheep 
 {
-    string _name { get; }
-    string _message { get; }
-    DateTimeOffset _date { get; }
+    public string _name { get; }
+    public string _message { get; }
+    public DateTimeOffset _date { get; }
 
     public static Cheep CheepFromString(string inputString)
     {
@@ -100,6 +113,16 @@ public record Cheep
     public override string ToString()
     {
         return $"{_name} @ {_date.DateTime}: {_message}";
+    }
+}
+
+public class CheepMap : ClassMap<Cheep>
+{
+    public CheepMap()
+    {
+        Map(m => m._name).Index(0).Name("name");
+        Map(m => m._message).Index(1).Name("message");
+        Map(m => m._date).Index(2).Name("date");
     }
 }
 
