681783a49da6208929ae19036ef9e67f716e3f27
Alex Tilgreen Mogensen <alext@itu.dk>
2024-09-10 12:17:08 +0200

    Added constructor and implemented the Read method'
    Co-authored-by: Rasmus <rarl@itu.dk>'

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,5 +1,47 @@
-﻿namespace Chirp.CLI.SimpleDB;
-public class Class1
-{
-    
+﻿using SimpleDB;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Text;
+using System.IO.Enumeration;
+using System.Globalization;
+
+namespace Chirp.CLI.SimpleDB;
+public class CSVDatabase : IDatabaseRepository<Cheep>
+{   
+    readonly String _fileName;
+    readonly CsvConfiguration _config;
+    readonly ClassMap<T> _classMap;
+
+    public CSVDatabase(string fileName, ClassMap<T> classMap) 
+    {
+        _fileName = fileName;
+        _classMap = classMap;
+        _config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true,
+            Encoding = Encoding.UTF8
+        };
+    }
+
+    public IEnumerable<Cheep> Read(int? limit = null)
+    {
+        List<Cheep> elements = new();
+        using (var reader = new StreamReader(_fileName, Encoding.UTF8))
+        using (var csv = new CsvReader(reader, config))
+        {
+            csv.Context.RegisterClassMap(classMap);
+            csv.Read();
+            csv.ReadHeader();
+            elements = csv.GetRecords<T>().ToList();
+        }
+        
+        amountToRead = Math.Min(elements.Count, amountToRead);
+        return elements.GetRange(elements.Count-amountToRead, amountToRead);
+    }
+    }
+
+    public void Store(Cheep record)
+    {
+        throw new NotImplementedException();
+    }
 }
