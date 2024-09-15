1ac812093db305ffbf33ca928ac44e24236fa66c
Mathias <mlao@itu.dk>
2024-09-15 10:38:18 +0200

    Create CheepCsvDatabase.cs

---
+++ Chirp/src/SimpleDB/CheepCsvDatabase.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper.Configuration;
+
+namespace SimpleDB;
+
+public sealed class CheepCsvDatabase<Cheep> : CsvDatabase<Cheep>
+{
+    private static readonly Lazy<CheepCsvDatabase<Cheep>> Lazy = new (() => new CheepCsvDatabase<Cheep>());
+    
+    public static CheepCsvDatabase<Cheep> Instance { get { return Lazy.Value; } }
+
+    private CheepCsvDatabase() : base("data/chirp_cli_db.csv", (new CheepMap() as ClassMap<Cheep>)!)
+    {
+    }
+}
