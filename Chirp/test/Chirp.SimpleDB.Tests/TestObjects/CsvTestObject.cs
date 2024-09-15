b5f6a856f7d80fc549a843903997653e5371c90e
stoftot <gryn@itu.dk>
2024-09-15 17:15:18 +0200

    Created TestObject
    crated a test object to better test the CsvDatabase methods
    and decouple the test from cheep

---
+++ Chirp/test/Chirp.SimpleDB.Tests/TestObjects/CsvTestObject.cs
@@ -0,0 +1,14 @@
+﻿namespace Chirp.SimpleDB.Tests;
+
+public class CsvTestObject
+{
+    public string filed1String { get; set; }
+    public int field2Int { get; set; }
+    public DateTime field3DataTime { get; set; }
+    public bool field4Bool { get; set; }
+    
+    public CsvTestObject()
+    {
+        
+    }
+}
