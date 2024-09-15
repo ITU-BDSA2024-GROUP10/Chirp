16ec24739c47449ddbfb5ef75f5621c84d4d4d15
stoftot <gryn@itu.dk>
2024-09-15 17:22:37 +0200

    Added equals for test object

--- Chirp/test/Chirp.SimpleDB.Tests/TestObjects/CsvTestObject.cs
+++ Chirp/test/Chirp.SimpleDB.Tests/TestObjects/CsvTestObject.cs
@@ -6,17 +6,28 @@ public class CsvTestObject
     public int field2Int { get; set; }
     public DateTime field3DataTime { get; set; }
     public bool field4Bool { get; set; }
-    
+
     public CsvTestObject()
     {
-        
     }
-    
+
     public CsvTestObject(string field1String, int field2Int, DateTime field3DataTime, bool field4Bool)
     {
         this.field1String = field1String;
         this.field2Int = field2Int;
         this.field3DataTime = field3DataTime;
         this.field4Bool = field4Bool;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        CsvTestObject other = (CsvTestObject)obj;
+        return field1String == other.field1String && field2Int == other.field2Int &&
+               field3DataTime == other.field3DataTime && field4Bool == other.field4Bool;
     }
 }
