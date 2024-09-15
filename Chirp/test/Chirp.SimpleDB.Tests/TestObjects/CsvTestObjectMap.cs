1e2f34a2c17492c04784e3e900d6164b79289757
stoftot <gryn@itu.dk>
2024-09-15 17:18:35 +0200

    Crated map for test object

---
+++ Chirp/test/Chirp.SimpleDB.Tests/TestObjects/CsvTestObjectMap.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper.Configuration;
+
+namespace Chirp.SimpleDB.Tests;
+
+public class CsvTestObjectMap : ClassMap<CsvTestObject>
+{
+    public CsvTestObjectMap()
+    {
+        Map(m => m.field1String).Index(0).Name("field1");
+        Map(m => m.field2Int).Index(1).Name("field2");
+        Map(m => m.field3DataTime).Index(2).Name("field3");
+        Map(m => m.field4Bool).Index(2).Name("field4");
+    }
+}
