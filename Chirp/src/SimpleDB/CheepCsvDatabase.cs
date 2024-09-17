8d9c61e76f11f4be656c5aadbf0dda3fa7d015e9
Mathias <mlao@itu.dk>
2024-09-17 09:30:54 +0200

    Removed reduntant code

--- Chirp/src/SimpleDB/CheepCsvDatabase.cs
+++ Chirp/src/SimpleDB/CheepCsvDatabase.cs
@@ -6,9 +6,9 @@ public sealed class CheepCsvDatabase : CsvDatabase<Cheep>
 {
     private static readonly Lazy<CheepCsvDatabase> Lazy = new (() => new CheepCsvDatabase());
     
-    public static CheepCsvDatabase Instance { get { return Lazy.Value; } }
+    public static CheepCsvDatabase Instance => Lazy.Value;
 
-    private CheepCsvDatabase() : base("data/chirp_cli_db.csv", (new CheepMap() as ClassMap<Cheep>)!)
+    private CheepCsvDatabase() : base("data/chirp_cli_db.csv", new CheepMap())
     {
     }
 }
