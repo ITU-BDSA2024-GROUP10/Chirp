d5aa21b6cec13c84ac14d2f4ec5cee18f760c480
anthoncastillo <145575420+anthoncastillo@users.noreply.github.com>
2024-09-26 15:56:49 +0200

    Implemented IDatabaseRepository method signatures
    To start work on SqlDB functionality
    
    Co-authored-by: Christoffer <gryn@itu.dk>

--- Chirp/src/SimpleDB/SQLiteDBFascade.cs
+++ Chirp/src/SimpleDB/SQLiteDBFascade.cs
@@ -1,6 +1,21 @@
-﻿namespace SimpleDB;
+﻿using System.Data.SqlClient;
 
-public class SQLiteDBFascade
+namespace SimpleDB;
+
+public class SQLiteDBFascade : IDatabaseRepository<Cheep>
 {
-    
+    private SqlConnection establishConnection()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Cheep> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Cheep> GetFromAuthor(int authorId)
+    {
+        throw new NotImplementedException();
+    }
 }
