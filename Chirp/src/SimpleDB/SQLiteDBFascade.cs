235af6502c3281034c7e8456f4f5b9a0c9b61ef3
anthoncastillo <145575420+anthoncastillo@users.noreply.github.com>
2024-09-29 19:52:39 +0200

    Implemented GetAll method
    Co-Authored-By: Christoffer grünberg <70943516+stoftot@users.noreply.github.com>

--- Chirp/src/SimpleDB/SQLiteDBFascade.cs
+++ Chirp/src/SimpleDB/SQLiteDBFascade.cs
@@ -17,7 +17,23 @@ public class SQLiteDBFascade : IDatabaseRepository<CheepViewModel>
 
     public IEnumerable<CheepViewModel> GetAll()
     {
-        throw new NotImplementedException();
+        var query = @"SELECT u.username, m.text, m.pup_date FROM message m
+                        JOIN user u ON m.author_id = u.user_id";
+        using (var connection = establishConnection())
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = query;
+            
+            List<CheepViewModel> result = new List<CheepViewModel>();
+            
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var cheep = new CheepViewModel(reader.GetString(0), reader.GetString(1), reader.GetString(2));
+                result.Add(cheep);
+            }
+            return result;
+        }
     }
 
     public IEnumerable<CheepViewModel> GetFromAuthor(int authorId)
