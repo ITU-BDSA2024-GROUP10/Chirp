007d85deaca1ca92b43150b1286e9f11327a8295
Christoffer Grünberg <gryn@itu.dk>
2024-10-02 15:53:04 +0200

    Update CheepService.cs
    Co-authored-by: Anthon <acah@itu.dk>
    Co-authored-by: Rasmus <rarl@itu.dk>
    Co-authored-by: Mathias <mlao@itu.dk>

--- Chirp/src/Chirp.Razor/CheepService.cs
+++ Chirp/src/Chirp.Razor/CheepService.cs
@@ -3,29 +3,17 @@ using SimpleDB.Model;
 
 public interface ICheepService
 {
-    public List<CheepDTO> GetAllCheeps();
     public List<CheepDTO> GetCheepsByPage(int page, int pageSize);
-    public List<CheepDTO> GetCheepsFromAuthor(string author);
     public List<CheepDTO> GetCheepsFromAuthorByPage(string author, int page, int pageSize);
 }
 
 public class CheepService : ICheepService
 {
     private IDatabaseRepository<CheepDTO> db = new SQLiteDBFascade();
-
-    public List<CheepDTO> GetAllCheeps()
-    {
-        return db.GetAll().ToList();
-    }
     
     public List<CheepDTO> GetCheepsByPage(int page, int pageSize)
     {
         return db.GetByPage(page, pageSize).ToList();
-    }
-
-    public List<CheepDTO> GetCheepsFromAuthor(string author)
-    {
-        return db.GetFromAuthor(author).ToList();
     }
     
     public List<CheepDTO> GetCheepsFromAuthorByPage(string author, int page, int pageSize)
