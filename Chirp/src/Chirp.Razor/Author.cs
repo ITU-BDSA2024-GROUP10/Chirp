427222af8796c1007fb610e5d57faf1b6b1b5e9f
Anthon <acah@itu.dk>
2024-10-02 11:07:08 +0200

    Added list of cheeps for Author class
    To keep track of an authors cheeps
    
    Co-authored-by: Christoffer <gryn@itu.dk>

--- Chirp/src/Chirp.Razor/Author.cs
+++ Chirp/src/Chirp.Razor/Author.cs
@@ -2,12 +2,15 @@ namespace Chirp.Razor;
 
 public class Author
 {
+    
     private string name;
     private string email;
+    private List<Cheep> listOfCheeps;
 
     public Author(string name, string email)
     {
         this.email = email;
         this.name = name;
+        listOfCheeps = new List<Cheep>();
     }
 }
