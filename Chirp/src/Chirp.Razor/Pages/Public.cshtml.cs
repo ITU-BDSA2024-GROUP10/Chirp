d67dc01651ef4420192c92d9b73ae11520c64c26
Mathias <mlao@itu.dk>
2024-10-02 10:08:31 +0200

    get query parameter pagenumber preparing for implementing pagenation
    Co-authored-by: Rasmus <rarl@itu.dk>

--- Chirp/src/Chirp.Razor/Pages/Public.cshtml.cs
+++ Chirp/src/Chirp.Razor/Pages/Public.cshtml.cs
@@ -8,14 +8,23 @@ public class PublicModel : PageModel
 {
     private readonly ICheepService _service;
     public List<CheepViewModel> Cheeps { get; set; }
+    
+    [BindProperty] 
+    public int PageNumber { get; set; }
 
     public PublicModel(ICheepService service)
     {
         _service = service;
     }
 
-    public ActionResult OnGet()
+    public ActionResult OnGet(string pageNumber)
     {
+        Console.WriteLine(pageNumber);
+        if (!string.IsNullOrEmpty(pageNumber))
+        {
+            PageNumber = int.Parse(pageNumber);
+            
+        }
         Cheeps = _service.GetCheeps();
         return Page();
     }
