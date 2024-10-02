9a358ca7800beac6a229f2960365d3ede371567a
Anthon <acah@itu.dk>
2024-10-02 14:40:15 +0200

    Added Id prop
    Co-Authored-By: Christoffer grünberg <70943516+stoftot@users.noreply.github.com>

--- Chirp/src/Chirp.Razor/DataModels/Author.cs
+++ Chirp/src/Chirp.Razor/DataModels/Author.cs
@@ -2,12 +2,16 @@ namespace Chirp.Razor.DataModels;
 
 public class Author
 {
+    private int Id { get; set; }
     private string Name { get; set; }
     private string Email { get; set; }
     private List<Cheep> ListOfCheeps { get; set; }
 
-    public Author(string name, string email)
     {
+    
+    public Author(int id, string name, string email)
+    {
+        Id = id;
         Email = email;
         Name = name;
         ListOfCheeps = new List<Cheep>();
