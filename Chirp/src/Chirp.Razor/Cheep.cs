def467a15f8d6dadfcc756c96eba24629fb157a6
Anthon <acah@itu.dk>
2024-10-02 11:03:11 +0200

    Added Cheep object class in Chirp.Razor

---
+++ Chirp/src/Chirp.Razor/Cheep.cs
@@ -0,0 +1,15 @@
+namespace Chirp.Razor;
+
+public class Cheep
+{
+    private string text;
+    private DateTime timeStamp;
+    private Author author;
+
+    public Cheep(string text, DateTime timeStamp, Author author)
+    {
+        this.text = text;
+        this.timeStamp = timeStamp;
+        this.author = author;
+    }
+}
