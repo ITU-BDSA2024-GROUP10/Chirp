1e541c41758c57157788d77d0a9eef947949e656
Mathias <mlao@itu.dk>
2024-10-02 15:07:19 +0200

    Get queryparams better, set page size = 32

--- Chirp/src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ Chirp/src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -8,23 +8,15 @@ public class UserTimelineModel : PageModel
 {
     private readonly ICheepService _service;
     public List<CheepViewModel> Cheeps { get; set; }
-    [BindProperty] 
-    public int PageNumber { get; set; }
 
     public UserTimelineModel(ICheepService service)
     {
         _service = service;
     }
 
-    public ActionResult OnGet(string author, string pageNumber)
+    public ActionResult OnGet(string author, [FromQuery] int page)
     {
-        if (!string.IsNullOrEmpty(pageNumber))
-        {
-            PageNumber = int.Parse(pageNumber);
-            Cheeps = _service.GetCheepsFromAuthorByPage(author, PageNumber, 12);
-            return Page();
-        }
-        Cheeps = _service.GetCheepsFromAuthor(author);
+        Cheeps = _service.GetCheepsFromAuthorByPage(author, page, 32);
         return Page();
     }
 }
