44b0a0f37665cc21bce5501b791e743508a94015
Mathias <mlao@itu.dk>
2024-10-02 11:23:09 +0200

    Added handling for pagenumber and pagenation

--- Chirp/src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ Chirp/src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -8,14 +8,22 @@ public class UserTimelineModel : PageModel
 {
     private readonly ICheepService _service;
     public List<CheepViewModel> Cheeps { get; set; }
+    [BindProperty] 
+    public int PageNumber { get; set; }
 
     public UserTimelineModel(ICheepService service)
     {
         _service = service;
     }
 
-    public ActionResult OnGet(string author)
+    public ActionResult OnGet(string author, string pageNumber)
     {
+        if (!string.IsNullOrEmpty(pageNumber))
+        {
+            PageNumber = int.Parse(pageNumber);
+            Cheeps = _service.GetCheepsFromAuthorByPage(author, PageNumber, 12);
+            return Page();
+        }
         Cheeps = _service.GetCheepsFromAuthor(author);
         return Page();
     }
