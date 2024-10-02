88958b8bec9c92d89e86c06cd74a3dd8f53c6d26
Anthon <acah@itu.dk>
2024-10-02 14:32:55 +0200

    Made fields into properties

--- Chirp/src/Chirp.Razor/DataModels/Cheep.cs
+++ Chirp/src/Chirp.Razor/DataModels/Cheep.cs
@@ -2,14 +2,14 @@ namespace Chirp.Razor.DataModels;
 
 public class Cheep
 {
-    private string text;
-    private DateTime timeStamp;
-    private Author author;
+    private string Text { get; set; }
+    private DateTime TimeStamp { get; set; }
+    private Author Author { get; set; }
 
     public Cheep(string text, DateTime timeStamp, Author author)
     {
-        this.text = text;
-        this.timeStamp = timeStamp;
-        this.author = author;
+        Text = text;
+        TimeStamp = timeStamp;
+        Author = author;
     }
 }
