cb32cecac72128029fc4585251742be1c6eb97c3
Mathias <mlao@itu.dk>
2024-10-02 10:24:12 +0200

    Added getByPage to IDatabaseRepository
    Co-authored-by: Rasmus <rarl@itu.dk>

--- Chirp/src/SimpleDB/IDatabaseRepository.cs
+++ Chirp/src/SimpleDB/IDatabaseRepository.cs
@@ -3,5 +3,6 @@
 public interface IDatabaseRepository<T>
 {
     public IEnumerable<T> GetAll();
+    public IEnumerable<T> GetByPage(int page, int pageSize);
     public IEnumerable<T> GetFromAuthor(String author);
 }
