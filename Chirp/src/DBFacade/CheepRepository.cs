c50c68dac4c7cb94b14665aab0f849604667295d
Mathias <mlao@itu.dk>
2024-10-03 14:07:54 +0200

    Implemented ICheepRepository, but missing DbContext
    Co-authored-by: Christoffer <gryn@itu.dk>
    Co-authored-by: Anthon <acah@itu.dk>
    Co-authored-by: Alex <alext@itu.dk>

--- Chirp/src/DBFacade/CheepRepository.cs
+++ Chirp/src/DBFacade/CheepRepository.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SimpleDB.Model;
 
 namespace SimpleDB;
@@ -8,13 +7,28 @@ public class CheepRepository : ICheepRepository
 {
     private DbContext context = null;
     
-    public Task<IEnumerable<CheepDTO>> GetCheepsByPage(int page, int pageSize)
+    public async Task<IEnumerable<CheepDTO>> GetCheepsByPage(int page, int pageSize)
     {
-        throw new NotImplementedException();
+        var query = context.Cheeps
+            .Select(cheep => new {cheep.Author.Name, cheep.message, cheep.timestamp})
+            .Skip((page-1)*pageSize)
+            .Take(pageSize);
+        
+        var cheeps = await query.AsEnumerable();
+        
+        return cheeps.map(cheep => new CheepDTO(cheep.name, cheep.message, cheep.timestamp));
     }
     
-    public Task<IEnumerable<CheepDTO>> GetCheepsFromAuthorByPage(String author, int page, int pageSize)
+    public async Task<IEnumerable<CheepDTO>> GetCheepsFromAuthorByPage(string author, int page, int pageSize)
     {
-        throw new NotImplementedException();
+        var query = context.Cheeps
+            .Where(cheep => cheep.Author.Name == author)
+            .Select(cheep => new {cheep.name, cheep.message, cheep.timestamp})
+            .Skip((page-1)*pageSize)
+            .Take(pageSize);
+        
+        var cheeps = await query.AsEnumerable();
+        
+        return cheeps.map(cheep => new CheepDTO(cheep.name, cheep.message, cheep.timestamp));
     }
 }
