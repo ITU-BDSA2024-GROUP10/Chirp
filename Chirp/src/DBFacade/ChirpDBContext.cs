ffef84f2463121916f861c6a883957373940cd1e
stoftot <gryn@itu.dk>
2024-10-07 18:15:22 +0200

    Created database relations constraints for ChirpContext

--- Chirp/src/DBFacade/ChirpDBContext.cs
+++ Chirp/src/DBFacade/ChirpDBContext.cs
@@ -11,4 +11,38 @@ public class ChirpDBContext : DbContext
     public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //define cheep entity constraints
+        modelBuilder.Entity<Cheep>(entity =>
+        {
+            //define auto increment key
+            entity.HasKey(c => c.Id);
+            entity.Property(c => c.Id).ValueGeneratedOnAdd();
+
+            //define required fields
+            entity.Property(c => c.Text).IsRequired();
+            entity.Property(c => c.TimeStamp).IsRequired();
+
+            //define foreign relation
+            entity.HasOne(c => c.Author)
+                .WithMany(a => a.ListOfCheeps)
+                .HasForeignKey(c => c.AuthorId);
+        });
+
+        //define author entity constraints
+        modelBuilder.Entity<Author>(entity =>
+        {
+            //define auto increment key
+            entity.HasKey(a => a.Id);
+            entity.Property(a => a.Id).ValueGeneratedOnAdd();
+
+            //define required fields
+            entity.Property(a => a.Name).IsRequired();
+            entity.Property(a => a.Email).IsRequired();
+        });
+    }
 }
