32cf2c94f8c2fce5f6b7c25b211f0f9ec43a0d71
stoftot <gryn@itu.dk>
2024-10-07 16:42:09 +0200

    Added models to context

--- Chirp/src/DBFacade/ChirpDBContext.cs
+++ Chirp/src/DBFacade/ChirpDBContext.cs
@@ -1,6 +1,14 @@
-﻿namespace SimpleDB;
+﻿using Chirp.Razor.DataModels;
+using Microsoft.EntityFrameworkCore;
 
-public class ChirpDBContext
+namespace SimpleDB;
+
+public class ChirpDBContext : DbContext
 {
-    
+    private DbSet<Cheep> Cheeps { get; set; }
+    private DbSet<Author> Authors { get; set; }
+
+    public ChirpDBContext(DbContextOptions<ChirpDBContext> options) : base(options)
+    {
+    }
 }
