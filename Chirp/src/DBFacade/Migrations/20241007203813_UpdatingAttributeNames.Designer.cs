186bf5d28834150207221f0f0a2ef35873613bb2
stoftot <gryn@itu.dk>
2024-10-07 22:40:50 +0200

    Migration with attribute name changes

--- Chirp/src/DBFacade/Migrations/20241007203813_UpdatingAttributeNames.Designer.cs
+++ Chirp/src/DBFacade/Migrations/20241007203813_UpdatingAttributeNames.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimpleDB;
 
@@ -10,9 +11,11 @@ using SimpleDB;
 namespace SimpleDB.Migrations
 {
     [DbContext(typeof(ChirpDBContext))]
-    partial class ChirpDBContextModelSnapshot : ModelSnapshot
+    [Migration("20241007203813_UpdatingAttributeNames")]
+    partial class UpdatingAttributeNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
