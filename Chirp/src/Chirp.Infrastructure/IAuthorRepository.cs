dcd67a889f105e7fb9a28484341a6dc857241400
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-10-09 14:04:44 +0200

    Added CreateAuthor method

--- Chirp/src/Chirp.Infrastructure/IAuthorRepository.cs
+++ Chirp/src/Chirp.Infrastructure/IAuthorRepository.cs
@@ -7,4 +7,6 @@ public interface IAuthorRepository
 {
     public Task<AuthorDTO> GetAuthorByName(string name); 
     public Task<AuthorDTO> GetAuthorByEmail(string email); 
+    
+    public Task<bool> AddAuthor(AuthorDTO author);
 }
