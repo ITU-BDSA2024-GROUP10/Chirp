462cc3915fd5fecf305add5957c9c2e734a6edfd
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-10-09 14:01:52 +0200

    Added GetAuthorByEmail method

--- Chirp/src/Chirp.Infrastructure/IAuthorRepository.cs
+++ Chirp/src/Chirp.Infrastructure/IAuthorRepository.cs
@@ -6,4 +6,5 @@ namespace SimpleDB;
 public interface IAuthorRepository
 {
     public Task<IEnumerable<AuthorDTO>> GetAuthorByName(String name); 
+    public Task<AuthorDTO> GetAuthorByEmail(String email); 
 }
