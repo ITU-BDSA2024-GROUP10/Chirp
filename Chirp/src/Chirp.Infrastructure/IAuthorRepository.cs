3e26ad353f9061ff1d2369b36ceacc145e22147e
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-10-09 13:58:55 +0200

    Created IAuthorRepository

--- Chirp/src/Chirp.Infrastructure/IAuthorRepository.cs
+++ Chirp/src/Chirp.Infrastructure/IAuthorRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace SimpleDB;
+﻿using Chirp.Razor.DataModels;
+
+namespace SimpleDB;
 
 public interface IAuthorRepository
 {
-    
+    public Task<IEnumerable<Author>> GetAuthorByName(String name); 
 }
