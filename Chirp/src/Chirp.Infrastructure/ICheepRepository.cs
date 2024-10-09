41bf0dd6eee9c802c867a2fd62a48daad4d6c8d6
Anthon <acah@itu.dk>
2024-10-09 14:46:44 +0200

    Added task bool to check if the insertion was successful or not

--- Chirp/src/Chirp.Infrastructure/ICheepRepository.cs
+++ Chirp/src/Chirp.Infrastructure/ICheepRepository.cs
@@ -7,5 +7,5 @@ public interface ICheepRepository
 {
     public Task<IEnumerable<CheepDTO>> GetCheepsByPage(int page, int pageSize);
     public Task<IEnumerable<CheepDTO>> GetCheepsFromAuthorByPage(String author, int page, int pageSize);
-    public void CreateCheep(CheepDTO cheep);
+    public Task<bool> CreateCheep(CheepDTO cheep);
 }
