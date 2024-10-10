088cd9a2d09707e1a3a0517c835e16d82cc43cb2
Anthon <acah@itu.dk>
2024-10-10 11:14:35 +0200

    Added empty test methods for getauthorbyname

--- Chirp/test/RepositoryTests/AuthorRepositoryUnitTest.cs
+++ Chirp/test/RepositoryTests/AuthorRepositoryUnitTest.cs
@@ -1,6 +1,27 @@
-﻿namespace RepositoryTests;
+﻿using Moq;
+using SimpleDB;
+
+namespace RepositoryTests;
 
 public class AuthorRepositoryUnitTest
 {
-    
+    [Fact]
+    public void GetAuthorByName_NameCantBeFound_ReturnErrorMessage()
+    {
+        //Arrange an arbitrary DB
+        
+        //Act scenario where you want to find a name that isn't in the DB
+        
+        //Assert Message that informs the unsuccessful found
+    }
+
+    [Fact]
+    public void GetAuthorByName_NameIsHelge_ReturnsAuthorDTOOfHelge()
+    {
+        //Arrange an arbitrary author with name 'Helge' and create arbitrary database to put up
+        
+        //Act a scenario where the repository can get an author by the name of 'Helge'
+        
+        //Assert the value of the arbitrary author that was arranged
+    }
 }
