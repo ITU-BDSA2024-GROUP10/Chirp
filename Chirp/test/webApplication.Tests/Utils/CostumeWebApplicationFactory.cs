d0b5ebf737a5ab301292c7f6e81a6c39f8163449
stoftot <gryn@itu.dk>
2024-10-19 12:19:05 +0200

    Implemented microsoft WebApplicationFactory
    This is a standard template from Microsoft for integration testing of web apps when using an in memory database.
    
    https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-8.0#customize-webapplicationfactory

--- Chirp/test/webApplication.Tests/Utils/CostumeWebApplicationFactory.cs
+++ Chirp/test/webApplication.Tests/Utils/CostumeWebApplicationFactory.cs
@@ -1,6 +1,50 @@
-﻿namespace webApplication.Tests.Utils;
+﻿using System.Data.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
-public class CostumeWebApplicationFactory
+namespace webApplication.Tests.Utils;
+
+//Adapted from microsofts test guide, for clarification:
+//https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-8.0#customize-webapplicationfactory
+public class CostumeWebApplicationFactory<TProgram> 
+    : WebApplicationFactory<TProgram>
+    where TProgram : class
 {
-    
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<ApplicationDbContext>));
+
+            services.Remove(dbContextDescriptor);
+
+            var dbConnectionDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbConnection));
+
+            services.Remove(dbConnectionDescriptor);
+
+            // Create open SqliteConnection so EF won't automatically close it.
+            services.AddSingleton<DbConnection>(container =>
+            {
+                var connection = new SqliteConnection("DataSource=:memory:");
+                connection.Open();
+
+                return connection;
+            });
+
+            services.AddDbContext<ApplicationDbContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseSqlite(connection);
+            });
+        });
+
+        builder.UseEnvironment("Development");
+    }
 }
