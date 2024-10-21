56d8f2b4934757eabfd553288df70209b0c11131
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-10-21 21:39:29 +0200

    Implemented Test

--- Chirp/test/RepositoryTests/AuthorRepositoryUnitTest.cs
+++ Chirp/test/RepositoryTests/AuthorRepositoryUnitTest.cs
@@ -11,5 +11,28 @@ public class AuthorRepositoryUnitTest
     
     [Fact]
     public async void GetAuthorByEmailTest()
+    {
+        using var connection = new SqliteConnection("Filename=:memory:");
+        await connection.OpenAsync();
+        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+        
+        using var context = new ChirpDBContext(builder.Options);
+        await context.Database.EnsureCreatedAsync();
+        
+        IAuthorRepository authorRepository = new AuthorRepository(context);
+
+        var author = new Author
+        {
+            Name = "Tester",
+            Email = "Tester@tmail.com"
+        };
+
+        context.Authors.Add(author);
+        await context.SaveChangesAsync();
+        
+        var returnAuthor = await authorRepository.GetAuthorByEmail("Tester@tmail.com");
+        Assert.NotNull(returnAuthor);
+        Assert.Equal("Tester", returnAuthor.Name);
+        Assert.Equal("Tester@tmail.com", returnAuthor.Email);
     }
 }
