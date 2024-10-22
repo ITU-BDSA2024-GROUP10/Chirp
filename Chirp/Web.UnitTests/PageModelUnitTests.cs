9e24c384175da3c51f65bb3fe40f0c1e8d4da4ab
stoftot <gryn@itu.dk>
2024-10-23 00:14:02 +0200

    Created unit test

---
+++ Chirp/Web.UnitTests/PageModelUnitTests.cs
@@ -0,0 +1,37 @@
+using Chirp.Web.Pages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moq;
+using SimpleDB.Model;
+
+namespace Web.UnitTests;
+
+public class PageModelUnitTests
+{
+    [Fact]
+    public void PublicTimeLine()
+    {
+        // Arrange
+        var cheeps = new List<CheepDTO>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            cheeps.Add(
+                new CheepDTO("mr. test",
+                    "test",
+                    new DateTimeOffset(DateTime.Now.AddHours(i)).ToUnixTimeSeconds()));
+        }
+
+        var serviceMock = new Mock<ICheepService>();
+        serviceMock.Setup(x => x.GetCheepsByPage(It.IsAny<int>(), It.IsAny<int>())).Returns(cheeps);
+
+        var pageModel = new PublicModel(serviceMock.Object);
+
+        // Act
+        var result = pageModel.OnGet(1);
+
+        // Assert
+        Assert.IsType<PageResult>(result);
+        Assert.Equal(10, pageModel.Cheeps.Count);
+        Assert.Equal(cheeps, pageModel.Cheeps);
+    }
+}
