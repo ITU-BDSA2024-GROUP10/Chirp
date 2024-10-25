55d0d1404a60ddd2391d362616db972705605ad4
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-10-25 14:38:52 +0200

    Create Authors a11 and a12
    Create new authors so Helge and Adrian can login to the service

--- Chirp/src/Chirp.Infrastructure/DbInitializer.cs
+++ Chirp/src/Chirp.Infrastructure/DbInitializer.cs
@@ -23,6 +23,8 @@ public static class DbInitializer
             var a10 = new Author() { Id = "10", Name = "Jacqualine Gilcoine", Email = "Jacqualine.Gilcoine@gmail.com", Cheeps = new List<Cheep>() };
 
             var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var a11 = new Author {UserName = "ropf@itu.dk", Email = "ropf@itu.dk", Cheeps = new List<Cheep>(), Name = "Helge"};
+            var a12 = new Author {UserName = "adho@itu.dk", Email = "adho@itu.dk", Cheeps = new List<Cheep>(), Name = "Adrian"};
             var authors = new List<Author>() { a1, a2, a3, a4, a5, a6, a7, a8, a9, a10};
 
             var c1 = new Cheep() { Id = 1, Author = a10, Message = "They were married in Chicago, with old Smith, and was expected aboard every day; meantime, the two went past me.", TimeStamp = DateTime.Parse("2023-08-01 13:14:37") };
