e0c81d9aa946c8df7e377fd8335f2fc0fc8185b3
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-10-25 14:42:43 +0200

    Set the username

--- Chirp/src/Chirp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Chirp/src/Chirp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -120,7 +120,7 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-
+                user.Name = Input.UserName;
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
