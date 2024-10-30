bacb6f9a7f9de4c04a00ec93f8630073ef2cff57
Mathias <mlao@itu.dk>
2024-10-30 14:10:25 +0100

    Created hasTitle test
    Co-Authored-By: Alex <79690147+AlexTMIT@users.noreply.github.com>

--- Chirp/test/PlaywrightTests/UnitTest1.cs
+++ Chirp/test/PlaywrightTests/UnitTest1.cs
@@ -1,6 +1,9 @@
+using System.Text.RegularExpressions;
+using Microsoft.Playwright.NUnit;
+
 namespace PlaywrightTests;
 
-public class Tests
+public class Tests : PageTest
 {
     [SetUp]
     public void Setup()
@@ -8,8 +11,11 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public async Task HasTitle()
     {
-        Assert.Pass();
+        await Page.GotoAsync("https://bdsa2024group10chirpremotedb-h3c8bne5cahweegw.northeurope-01.azurewebsites.net/");
+
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Chirp!"));
     }
 }
