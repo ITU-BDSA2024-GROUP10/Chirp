b119e63906c981cebb1f25ef001d1c147d1eda08
Mathias <mlao@itu.dk>
2024-10-30 14:39:44 +0100

    Created HasRegisterLink test
    Co-Authored-By: Alex <79690147+AlexTMIT@users.noreply.github.com>

--- Chirp/test/PlaywrightTests/UnitTest1.cs
+++ Chirp/test/PlaywrightTests/UnitTest1.cs
@@ -22,4 +22,17 @@ public class Tests : PageTest
         // Expect a title "to contain" a substring.
         await Expect(Page).ToHaveTitleAsync(new Regex("Chirp!"));
     }
+    
+    [Test]
+    public async Task HasRegisterLink()
+    {
+        await Page.GotoAsync(BaseUrl);
+
+        // Click the get started link.
+        await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
+
+        await Expect(Page).ToHaveTitleAsync(new Regex("Register"));
+        // Expects page to have a heading with the name of Installation.
+        await Expect(Page.GetByRole(AriaRole.Form, new() { Name = "Installation" })).ToBeVisibleAsync();
+    }
 }
