574aeea70a4b344922f85d7c41edeb478b713d49
Mathias <mlao@itu.dk>
2024-11-01 15:07:19 +0100

    Moved UI tests

--- Chirp/test/PlaywrightTests/EndToEndTests.cs
+++ Chirp/test/PlaywrightTests/EndToEndTests.cs
@@ -7,44 +7,6 @@ namespace PlaywrightTests;
 [TestFixture]
 public class EndToEndTests : PageTestWithCustomWebApplicationFactory
 {
-    [Test]
-    public async Task HasTitle()
-    {
-        await Page.GotoAsync("/");
-        await Expect(Page).ToHaveTitleAsync(new Regex("Chirp!"));
-    }
-
-    [Test]
-    public async Task HasRegisterLink()
-    {
-        await Page.GotoAsync("/");
-
-        // Click the get started link.
-        await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
-
-        await Expect(Page).ToHaveTitleAsync(new Regex("Register"));
-        // Expects page to have a heading with the name of Installation.
-        //await Expect(Page.GetByRole(AriaRole.Form, new() { Name = "Installation" })).ToBeVisibleAsync();
-    }
-
-    [Test]
-    public async Task PasswordMustHaveNonAlphanumericCharacters()
-    {
-        //act
-        await Page.GotoAsync("/Identity/Account/Register");
-        await Page.GetByPlaceholder("name", new() { Exact = true }).ClickAsync();
-        await Page.GetByPlaceholder("name", new() { Exact = true }).FillAsync("Mathias");
-        await Page.GetByPlaceholder("name@example.com").ClickAsync();
-        await Page.GetByPlaceholder("name@example.com").FillAsync("mlao@itu.dk");
-        await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
-        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Password123");
-        await Page.GetByLabel("Confirm Password").ClickAsync();
-        await Page.GetByLabel("Confirm Password").FillAsync("Password123");
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
-
-        //assert
-        await Expect(Page.GetByText("Passwords must have at least")).ToBeVisibleAsync();
-    }
 
     [Test]
     public async Task EndToEnd_RegisterLoginAndLogout()
--- Chirp/test/PlaywrightTests/UITest.cs
+++ Chirp/test/PlaywrightTests/UITest.cs
@@ -1,6 +1,45 @@
+using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+
 namespace PlaywrightTests;
 
-public class UITest
+public class UITest : PageTestWithCustomWebApplicationFactory
 {
-    
+    public async Task HasTitle()
+    {
+        await Page.GotoAsync("/");
+        await Expect(Page).ToHaveTitleAsync(new Regex("Chirp!"));
+    }
+
+    [Test]
+    public async Task HasRegisterLink()
+    {
+        await Page.GotoAsync("/");
+
+        // Click the get started link.
+        await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
+
+        await Expect(Page).ToHaveTitleAsync(new Regex("Register"));
+        // Expects page to have a heading with the name of Installation.
+        //await Expect(Page.GetByRole(AriaRole.Form, new() { Name = "Installation" })).ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task PasswordMustHaveNonAlphanumericCharacters()
+    {
+        //act
+        await Page.GotoAsync("/Identity/Account/Register");
+        await Page.GetByPlaceholder("name", new() { Exact = true }).ClickAsync();
+        await Page.GetByPlaceholder("name", new() { Exact = true }).FillAsync("name");
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("name@example.com");
+        await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
+        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Password123");
+        await Page.GetByLabel("Confirm Password").ClickAsync();
+        await Page.GetByLabel("Confirm Password").FillAsync("Password123");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
+
+        //assert
+        await Expect(Page.GetByText("Passwords must have at least")).ToBeVisibleAsync();
+    }
 }
