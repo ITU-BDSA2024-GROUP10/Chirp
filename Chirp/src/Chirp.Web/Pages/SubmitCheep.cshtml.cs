2412177d80cf246e5f4e44350d963ccf51aad102
stoftot <gryn@itu.dk>
2024-11-03 18:11:00 +0100

    Fixed warning, check if message is null, display error
    Added a check to see if the message is null and if so display an error back to the user. Supressed the possible null warning later down, since it wouldent get to that point if it was null.
    
    Warning:
    src\Chirp.Web\Pages\SubmitCheep.cshtml.cs(21,13): warning CS8604: Possible null reference argument
     for parameter 'message' in 'CheepDTO.CheepDTO(string author, string message, long unixTimestamp)'

--- Chirp/src/Chirp.Web/Pages/SubmitCheep.cshtml.cs
+++ Chirp/src/Chirp.Web/Pages/SubmitCheep.cshtml.cs
@@ -11,14 +11,18 @@ public class SubmitCheepModel(ICheepService service) : PageModel {
     
     public ActionResult OnPost(MessageModel messageModel)
     {
+        if (string.IsNullOrWhiteSpace(messageModel.Message)) {
+            ModelState.AddModelError("Message", "Message cannot be empty");
+        }
+        
         if (!ModelState.IsValid) {
             return Page();
         }
-
+        
         var dt = (DateTimeOffset)DateTime.UtcNow;
         var cheep = new CheepDTO (
             User.Claims.FirstOrDefault(c => c.Type == "UserName")?.Value ?? "no name",
-            messageModel.Message,
+            messageModel.Message!,
             dt.ToUnixTimeSeconds()
         );
 
