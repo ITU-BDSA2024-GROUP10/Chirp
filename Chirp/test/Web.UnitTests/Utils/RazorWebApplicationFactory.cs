bfa32b574c21715f0d6b9683b1d252a2a971e6b8
Christoffer Grünberg <gryn@itu.dk>
2024-11-05 13:33:05 +0100

    Changed razorWebApllicationFactory to use in memory database

--- Chirp/test/Web.UnitTests/Utils/RazorWebApplicationFactory.cs
+++ Chirp/test/Web.UnitTests/Utils/RazorWebApplicationFactory.cs
@@ -1,7 +1,11 @@
-﻿using Chirp.Core;
+﻿using System.Data.Common;
+using Chirp.Core;
+using Chirp.Infrastructure;
 using Chirp.Web;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -14,6 +18,35 @@ public class RazorWebApplicationFactory<TProgram>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.ConfigureServices(services =>
+        {
+            var dbContextDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<ChirpDBContext>));
+            if (dbContextDescriptor != null) services.Remove(dbContextDescriptor);
+
+            var dbConnectionDescriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbConnection));
+
+            if (dbConnectionDescriptor != null) services.Remove(dbConnectionDescriptor);
+
+            // Create open SqliteConnection so EF won't automatically close it.
+            services.AddSingleton<DbConnection>(container =>
+            {
+                var connection = new SqliteConnection("DataSource=:memory:");
+                connection.Open();
+
+                return connection;
+            });
+
+            services.AddDbContext<ChirpDBContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseSqlite(connection);
+            });
+        });
+        
         builder.ConfigureAppConfiguration((_, config) =>
         {
             var testConfiguration = new Dictionary<string, string>
