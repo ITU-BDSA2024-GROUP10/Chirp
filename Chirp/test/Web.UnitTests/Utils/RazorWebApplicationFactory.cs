6b8c0fe4a364319e2c17f1fc8e4e972fad0ca077
Christoffer Grünberg <gryn@itu.dk>
2024-11-05 13:49:21 +0100

    Updated webUnitTest to use new factory

--- Chirp/test/Web.UnitTests/Utils/RazorWebApplicationFactory.cs
+++ Chirp/test/Web.UnitTests/Utils/RazorWebApplicationFactory.cs
@@ -9,57 +9,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using TestUtils;
 
 namespace Web.UnitTests.Utils;
 
 public class RazorWebApplicationFactory<TProgram>
-    : WebApplicationFactory<TProgram>
+    : InMemoryCostumeWebApplicationFactory<TProgram>
     where TProgram : class
 {
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        builder.ConfigureServices(services =>
-        {
-            var dbContextDescriptor = services.SingleOrDefault(
-                d => d.ServiceType ==
-                     typeof(DbContextOptions<ChirpDBContext>));
-            if (dbContextDescriptor != null) services.Remove(dbContextDescriptor);
-
-            var dbConnectionDescriptor = services.SingleOrDefault(
-                d => d.ServiceType ==
-                     typeof(DbConnection));
-
-            if (dbConnectionDescriptor != null) services.Remove(dbConnectionDescriptor);
-
-            // Create open SqliteConnection so EF won't automatically close it.
-            services.AddSingleton<DbConnection>(container =>
-            {
-                var connection = new SqliteConnection("DataSource=:memory:");
-                connection.Open();
-
-                return connection;
-            });
-
-            services.AddDbContext<ChirpDBContext>((container, options) =>
-            {
-                var connection = container.GetRequiredService<DbConnection>();
-                options.UseSqlite(connection);
-            });
-        });
-        
-        builder.ConfigureAppConfiguration((_, config) =>
-        {
-            var testConfiguration = new Dictionary<string, string>
-            {
-                ["authentication:github:clientId"] = "test-client-id",
-                ["authentication:github:clientSecret"] = "test-client-secret"
-            };
-            config.AddInMemoryCollection(testConfiguration!);
-        });
-        
-        builder.UseEnvironment("Testing");
-    }
-
     public HttpClient GetClientFromCheepServiceMock(Mock<ICheepService> serviceMock)
     {
         return WithWebHostBuilder(builder =>
