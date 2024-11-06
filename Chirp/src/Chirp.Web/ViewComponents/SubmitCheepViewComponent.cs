7f925c2550752c70b4e0c0c663e40504c1a2c9b5
stoftot <gryn@itu.dk>
2024-11-06 11:48:14 +0100

    Updated viewComponent to take a page
    This is to deliver it to the default chrip box view

--- Chirp/src/Chirp.Web/ViewComponents/SubmitCheepViewComponent.cs
+++ Chirp/src/Chirp.Web/ViewComponents/SubmitCheepViewComponent.cs
@@ -3,11 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 
 public class SubmitCheepViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke(MessageModel messageModel)
+    public IViewComponentResult Invoke(MessageModel messageModel, string targetPage)
     {
         if (!User.Identity!.IsAuthenticated) {
             return View("Unauthenticated");
         }
+        ViewBag.TargetPage = targetPage;
         return View("Default", messageModel);
     }
 }
