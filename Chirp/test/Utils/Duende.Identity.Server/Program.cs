95eea063bba6663d89817fd8c97b27e168f4a442
stoftot <gryn@itu.dk>
2024-11-11 09:47:56 +0100

    Created Duende identity project, from inMem template
    The only files that have been modified from the template are.
    The config, the testUsers, program.cs, and the lauch settings, to make it launch over http.
    
    Template:
    https://github.com/DuendeSoftware/IdentityServer.Templates?tab=readme-ov-file#dotnet-new-isinmem
    
    Overview over templates:
    https://docs.duendesoftware.com/identityserver/v7/quickstarts/0_overview/

---
+++ Chirp/test/Utils/Duende.Identity.Server/Program.cs
@@ -0,0 +1,55 @@
+﻿using Duende.Identity.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// Configure IdentityServer with in-memory stores, keys, clients, and scopes
+builder.Services.AddIdentityServer(options =>
+    {
+        options.EmitStaticAudienceClaim = true;
+    })
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddTestUsers(TestUsers.Users);
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.SameSite = SameSiteMode.Lax;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest; // Adjusts cookie secure policy
+});
+
+var app = builder.Build();
+
+app.UseCookiePolicy(new CookiePolicyOptions
+{
+    MinimumSameSitePolicy = SameSiteMode.Lax,
+});
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+namespace Duende.Identity.Server
+{
+    public partial class Program
+    {
+    }
+}
