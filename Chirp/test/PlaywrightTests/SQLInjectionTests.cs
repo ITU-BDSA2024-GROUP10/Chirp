12a1f558e0cf1cff12599bb31ea6cbc704f2bc80
Anthon <acah@itu.dk>
2024-11-12 13:51:45 +0100

    Added test for SQL Injection
    Co-Authored-By: Mathias <62756914+math045b@users.noreply.github.com>
    Co-Authored-By: Christoffer grünberg <70943516+stoftot@users.noreply.github.com>

---
+++ Chirp/test/PlaywrightTests/SQLInjectionTests.cs
@@ -0,0 +1,38 @@
+using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+
+namespace PlaywrightTests;
+
+public class SQLInjectionTests : PageTestWithCustomWebApplicationFactory
+{
+    [Test]
+    public async Task SQLInjection()
+    {
+        //act
+        await Page.GotoAsync("/");
+        await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
+        await Page.GetByPlaceholder("name", new() { Exact = true }).ClickAsync();
+        await Page.GetByPlaceholder("name", new() { Exact = true }).FillAsync("Mathias");
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("mlao@itu.dk");
+        await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
+        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Password123!");
+        await Page.GetByLabel("Confirm Password").ClickAsync();
+        await Page.GetByLabel("Confirm Password").FillAsync("Password123!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" }).ClickAsync();
+        await Expect(Page.GetByText("Thank you for confirming your")).ToBeVisibleAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").ClickAsync();
+        await Page.GetByPlaceholder("name@example.com").FillAsync("mlao@itu.dk");
+        await Page.GetByPlaceholder("password").ClickAsync();
+        await Page.GetByPlaceholder("password").FillAsync("Password123!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        await Page.Locator("#Message").ClickAsync();
+        await Page.Locator("#Message").FillAsync("105; DROP TABLE Cheeps;");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
+        
+        //assert
+        await Expect(Page.Locator("#messagelist")).ToContainTextAsync("105; DROP TABLE Cheeps;");
+    }
+}
