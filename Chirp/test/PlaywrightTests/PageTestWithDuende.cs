7bdc4b8d433284c8e1bc0a66e1842e9b0778268e
Christoffer Grünberg <gryn@itu.dk>
2024-11-12 12:23:09 +0100

    Updated pageTestWithDuende to extende other pagetest

--- Chirp/test/PlaywrightTests/PageTestWithDuende.cs
+++ Chirp/test/PlaywrightTests/PageTestWithDuende.cs
@@ -7,61 +7,46 @@ using Microsoft.Playwright.NUnit;
 
 namespace PlaywrightTests;
 
-public class PageTestWithDuende : PlaywrightTest
+public class PageTestWithDuende : PageTestWithCustomWebApplicationFactory
 {
     protected const string DuendeBaseUrl = "http://localhost:5001";
-    protected const string RazorBaseUrl = "http://localhost:5273";
-
-    //private IPlaywright _playwright;
-    private IBrowser _browser;
-    protected IPage Page { get; private set; }
 
     private DuendePlaywrightWebAppFactory _duendeFactory;
-    private CustomWebApplicationFactory _razorFactory;
 
     private HttpClient _duendeClient;
-    private HttpClient _razorClient;
-
+    
+    public override BrowserNewContextOptions ContextOptions()
+    {
+        return new BrowserNewContextOptions()
+        {
+            Locale = "en-US",
+            ColorScheme = ColorScheme.Light
+        };
+    }
+    
     [OneTimeSetUp]
-    public async Task OneTimeSetUp()
+    public void DuendeOneTimeSetUp()
     {
         _duendeFactory = new DuendePlaywrightWebAppFactory(DuendeBaseUrl);
-        _razorFactory = new CustomWebApplicationFactory(RazorBaseUrl);
-        
-        //_playwright = await Playwright.CreateAsync();
-        
-        //_browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
-        
     }
 
     [SetUp]
-    public async Task Setup()
+    public void DuendeSetup()
     {
-        //for pop with bowser that does the steps, set headless to false, useful for debugging
-        _browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
-        
-        var Context = await _browser.NewContextAsync();
-        Page = await Context.NewPageAsync();
-        
         _duendeClient = _duendeFactory.CreateClient();
-        _razorClient = _razorFactory.WithWebHostBuilder(builder => builder.UseUrls(RazorBaseUrl)).CreateClient();
     }
 
     [TearDown]
-    public async Task Teardown()
+    public async Task DuendeTeardown()
     {
         _duendeClient.Dispose();
-        _razorClient.Dispose();
         await Page.CloseAsync();
     }
 
     [OneTimeTearDown]
-    public async Task OneTimeTearDown()
+    public async Task DuendeOneTimeTearDown()
     {
         _duendeClient.Dispose();
-        _razorClient.Dispose();
         await _duendeFactory.DisposeAsync();
-        await _razorFactory.DisposeAsync();
-        await _browser.CloseAsync();
     }
 }
