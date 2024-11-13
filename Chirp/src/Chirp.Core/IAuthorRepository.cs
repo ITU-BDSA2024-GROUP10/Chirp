1c245b2bce0d898c4f3d45affef15ce4cefe21ae
Anthon <acah@itu.dk>
2024-11-13 15:16:54 +0100

    removed unnecessary method
    Co-Authored-By: RasmusLarsen02 <105587694+RasmusLarsen02@users.noreply.github.com>
    Co-Authored-By: Christoffer grünberg <70943516+stoftot@users.noreply.github.com>
    Co-Authored-By: brycerkr <181973395+brycerkr@users.noreply.github.com>
    Co-Authored-By: Mathias <62756914+math045b@users.noreply.github.com>

--- Chirp/src/Chirp.Core/IAuthorRepository.cs
+++ Chirp/src/Chirp.Core/IAuthorRepository.cs
@@ -5,7 +5,6 @@ namespace Chirp.Core;
 public interface IAuthorRepository
 {
     public Task<AuthorDTO?> GetAuthorByName(string name);
-    public Task<AuthorDTO?> GetAuthorByEmail(string email);
-
+    
     public Task<bool> AddAuthor(AuthorDTO author);
 }
