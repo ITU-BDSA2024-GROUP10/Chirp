bb6d45f5cd6976f0d52731d51adb2179239e309f
Christoffer Grünberg <gryn@itu.dk>
2024-11-13 17:59:29 +0100

    Updated CostumeFactory To use shared db connection
    The db connection string "DataSource=file::memory:?cache=shared" is used to create an in-memory database, that is shared between the test host and the Kestrel host.

--- Chirp/test/PlaywrightTests/CustomWebApplicationFactory.cs
+++ Chirp/test/PlaywrightTests/CustomWebApplicationFactory.cs
@@ -15,8 +15,10 @@ namespace PlaywrightTests;
 //https://learn.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-8.0#customize-webapplicationfactory
 //Further Adapted from this Medium posts:
 //https://medium.com/younited-tech-blog/end-to-end-test-a-blazor-app-with-playwright-part-3-48c0edeff4b6
-public class CustomWebApplicationFactory 
-    : InMemoryCostumeWebApplicationFactory<Chirp.Web.Program>
+//The db connection string "DataSource=file::memory:?cache=shared" is used to create an in-memory database
+//that is shared between the test host and the Kestrel host.
+public class CustomWebApplicationFactory()
+    : InMemoryCostumeWebApplicationFactory<Chirp.Web.Program>("DataSource=file::memory:?cache=shared")
 {
     protected override IHost CreateHost(IHostBuilder builder)
     {
@@ -35,4 +37,36 @@ public class CustomWebApplicationFactory
         // call and relay to our HTTP host.
         return new CompositeHost(testHost, host);
     }
+    
+    //This is overriding the default ResetDB method from InMemoryCostumeWebApplicationFactory
+    //because when the db is shared between the test host and the Kestrel host
+    //using ensure deleted, wont clear the database properly.
+    //Therefore, we use this to manually delete all data from the database.
+    public new void ResetDB()
+    {
+        using (var scope = Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<ChirpDBContext>();
+
+            // Disable foreign key constraints temporarily to allow deletion in any order
+            context.Database.ExecuteSqlRaw("PRAGMA foreign_keys = OFF;");
+
+            // Iterate over all entity types and delete data
+            foreach (var entityType in context.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                //Its warning agianst potential SQL injection, but we are not taking input from user,
+                //so it's safe to ignore this warning
+#pragma warning disable EF1002
+                context.Database.ExecuteSqlRaw($"DELETE FROM \"{tableName}\";");
+#pragma warning restore EF1002
+            }
+
+            // Re-enable foreign key constraints
+            context.Database.ExecuteSqlRaw("PRAGMA foreign_keys = ON;");
+
+            context.SaveChanges();
+        }
+    }
+
 }
