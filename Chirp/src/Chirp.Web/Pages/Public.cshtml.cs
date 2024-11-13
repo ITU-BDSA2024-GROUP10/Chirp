b3fd8f5dbfb717d30f52d6f542b718f77285ece4
Mathias <mlao@itu.dk>
2024-11-13 20:13:59 +0100

    Moved pageNumber into TimelinePageModel & fixed  usertimeline redirected to public timeline

--- Chirp/src/Chirp.Web/Pages/Public.cshtml.cs
+++ Chirp/src/Chirp.Web/Pages/Public.cshtml.cs
@@ -1,23 +1,20 @@
 ﻿using Chirp.Core;
-using Chirp.Core.DTO;
 using Chirp.Web.Pages.Shared.Components.Timelines;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
 
 public class PublicModel(ICheepService service) : TimeLinePageModel(service)
 {
-    public int PageNumber;
     public ActionResult OnGet([FromQuery] int page)
     {
-        LoadCheeps(page);
+        PageNumber = page < 1 ? 1 : page;
+        LoadCheeps(PageNumber);
         return Page();
     }
 
     protected override void LoadCheeps(int page)
     {
-        PageNumber = page < 1 ? 1 : page;
-        Cheeps = Service.GetCheepsByPage(PageNumber, 32);
+        Cheeps = Service.GetCheepsByPage(page, 32);
     }
 }
--- Chirp/src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ Chirp/src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,27 +1,26 @@
 ﻿using Chirp.Core;
-using Chirp.Core.DTO;
 using Chirp.Web.Pages.Shared.Components.Timelines;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
 
 public class UserTimelineModel(ICheepService service) : TimeLinePageModel(service)
 {
-    public int PageNumber;
     public ActionResult OnGet(string author, [FromQuery] int page)
     {
         Author = author;
+        PageNumber = page < 1 ? 1 : page;
         LoadCheeps(page);
         return Page();
     }
 
     protected override void LoadCheeps(int page)
     {
-        if (Author == null) {
+        if (Author == null)
+        {
             throw new ArgumentNullException(nameof(Author));
         }
-        PageNumber = page < 1 ? 1 : page;
-        Cheeps = Service.GetCheepsFromAuthorByPage(Author, PageNumber, 32);
+
+        Cheeps = Service.GetCheepsFromAuthorByPage(Author, page, 32);
     }
 }
