7afcb0559db0f67dbdf4f423ec456054c1e37add
Mathias <mlao@itu.dk>
2024-11-13 15:39:03 +0100

    Added buttons to public timeline

--- Chirp/src/Chirp.Web/Pages/Public.cshtml.cs
+++ Chirp/src/Chirp.Web/Pages/Public.cshtml.cs
@@ -8,6 +8,7 @@ namespace Chirp.Web.Pages;
 
 public class PublicModel(ICheepService service) : TimeLinePageModel(service)
 {
+    public int PageNumber;
     public ActionResult OnGet([FromQuery] int page)
     {
         LoadCheeps(page);
@@ -16,6 +17,7 @@ public class PublicModel(ICheepService service) : TimeLinePageModel(service)
 
     protected override void LoadCheeps(int page)
     {
-        Cheeps = Service.GetCheepsByPage(page, 32);
+        PageNumber = page < 1 ? 1 : page;
+        Cheeps = Service.GetCheepsByPage(PageNumber, 32);
     }
 }
