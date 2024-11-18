3c9f1e2254cad25f5c4ea54593ab38bc48d43a49
stoftot <gryn@itu.dk>
2024-11-18 15:17:19 +0100

    Extracted shared cheep list logic into component

---
+++ Chirp/src/Chirp.Web/ViewComponents/CheepListViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Chirp.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chirp.Web.ViewComponents;
+
+public class CheepListViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(IEnumerable<CheepDTO> cheeps)
+    {
+        ViewBag.Cheeps = cheeps;
+        return View("Default");
+    }
+}
