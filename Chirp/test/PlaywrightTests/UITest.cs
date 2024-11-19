b3b7b1b965b0e5569e214b43e5714fb75a421952
Mathias <mlao@itu.dk>
2024-11-19 22:34:08 +0100

    Fixed test
    Co-Authored-By: Christoffer grünberg <70943516+stoftot@users.noreply.github.com>

--- Chirp/test/PlaywrightTests/UITest.cs
+++ Chirp/test/PlaywrightTests/UITest.cs
@@ -69,6 +69,17 @@ public class UITest : PageTestWithRazorPlaywrightWebApplicationFactory
     [TestCase("test?page=1", null)]
     public async Task PaginationOfUserTimeline(string expectedEndpoint, string page)
     {
+        //arrange
+        var context = razorFactory.GetDbContext();
+        Author testAuthor = new Author
+        {
+            UserName = "test",
+            Email = "test@test.com",
+            NormalizedUserName = "TEST",
+        };
+        context.Authors.Add(testAuthor);
+        await context.SaveChangesAsync();
+        
         //act
         await Page.GotoAsync($"/test?page={page}");
 
