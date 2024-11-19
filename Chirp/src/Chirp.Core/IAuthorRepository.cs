a3e21b14b5af7c98363dc8063c0f55022c961c55
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-11-19 10:22:41 +0100

    Added method signature for GetFollows

--- Chirp/src/Chirp.Core/IAuthorRepository.cs
+++ Chirp/src/Chirp.Core/IAuthorRepository.cs
@@ -7,4 +7,6 @@ public interface IAuthorRepository
     public Task<AuthorDTO?> GetAuthorByName(string name);
     
     public Task<bool> AddAuthor(AuthorDTO author);
+    
+    public Task<List<AuthorDTO>> GetFollows(string username);
 }
