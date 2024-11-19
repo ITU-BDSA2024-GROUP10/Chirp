eb1834ee8557601daf735d5b436feb12ba19c74c
Mathias <mlao@itu.dk>
2024-11-19 16:14:37 +0100

    Made usertimeline get cheeps from followers
    Co-Authored-By: RasmusLarsen02 <105587694+RasmusLarsen02@users.noreply.github.com>
    Co-Authored-By: anthoncastillo <145575420+anthoncastillo@users.noreply.github.com>
    Co-Authored-By: brycerkr <181973395+brycerkr@users.noreply.github.com>
    Co-Authored-By: Christoffer grünberg <70943516+stoftot@users.noreply.github.com>

--- Chirp/src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ Chirp/src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -6,6 +6,7 @@ namespace Chirp.Web.Pages;
 
 public class UserTimelineModel(ICheepService cheepService, IAuthorService authorService) : TimeLinePageModel(cheepService)
 {
+    protected readonly IAuthorService AuthorService = authorService;
     public ActionResult OnGet(string author, [FromQuery] int page)
     {
         if (page < 1)
@@ -29,6 +30,7 @@ public class UserTimelineModel(ICheepService cheepService, IAuthorService author
             throw new ArgumentNullException(nameof(author), "Author cannot be null, failed to get the route value");
         }
 
-        Cheeps = Service.GetCheepsFromAuthorByPage(author, page, 32);
+        var authors = AuthorService.GetFollows(author).Select(a => a.Name).Append(author);
+        Cheeps = CheepService.GetCheepsFromAuthorsByPage(authors, page, 32);
     }
 }
