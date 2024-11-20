faa8ba45bd4d80831a548fea39db504791951e97
Christoffer Grünberg <gryn@itu.dk>
2024-11-20 12:44:02 +0100

    Gave user does not exsits an empty constructor

--- Chirp/src/Chirp.Core/CustomException/UserDoesNotExist.cs
+++ Chirp/src/Chirp.Core/CustomException/UserDoesNotExist.cs
@@ -2,5 +2,7 @@ namespace Chirp.Core.CustomException;
 
 public class UserDoesNotExist(string message) : Exception(message)
 {
-    
+    public UserDoesNotExist() : this("User does not exist")
+    {
+    }   
 }
