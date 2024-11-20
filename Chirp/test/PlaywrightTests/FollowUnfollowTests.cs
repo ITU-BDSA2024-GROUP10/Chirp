2c4895205a4dd0a09a3f09c4e11cc9cee178c2ec
Alex Tilgreen Mogensen <alext@itu.dk>
2024-11-20 12:21:41 +0100

    setup of before each with login

--- Chirp/test/PlaywrightTests/FollowUnfollowTests.cs
+++ Chirp/test/PlaywrightTests/FollowUnfollowTests.cs
@@ -2,6 +2,9 @@ using System.Text.RegularExpressions;
 using Chirp.Infrastructure.Model;
 using Microsoft.Playwright;
 using PlaywrightTests.Utils;
+using System;
+using System.Threading.Tasks;
+
 
 namespace PlaywrightTests;
 
@@ -10,6 +13,25 @@ namespace PlaywrightTests;
 [NonParallelizable]
 public class FollowUnfollowTests : PageTestWithRazorPlaywrightWebApplicationFactory
 {
+    [SetUp]
+    public async Task SetUp()
+    {
+        // navigate to the page
+        await Page.GotoAsync("http://localhost:5273/?page=1");
+
+        // login
+        await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+        await Page.GetByPlaceholder("Username").ClickAsync();
+        await Page.GetByPlaceholder("Username").FillAsync("testuser");
+        await Page.GetByPlaceholder("Username").PressAsync("Tab");
+        await Page.GetByPlaceholder("password").FillAsync("Test1234!");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+
+        // assert login success by checking present of my timeline
+        var myTimelineLink = Page.GetByRole(AriaRole.Link, new() { Name = "my timeline" });
+        await Assertions.Expect(myTimelineLink).ToBeVisibleAsync();
+    }
+    
     [Test]
     public async Task UserCanFollowAuthor()
     {
