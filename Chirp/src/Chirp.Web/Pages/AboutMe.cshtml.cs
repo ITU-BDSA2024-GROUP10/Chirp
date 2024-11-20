509c5e1fa2ca27b455141e548de044e54ab735b0
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-11-20 15:52:55 +0100

    Added AboutMe pagemodel

---
+++ Chirp/src/Chirp.Web/Pages/AboutMe.cshtml.cs
@@ -0,0 +1,18 @@
+using Chirp.Core;
+using Chirp.Core.DTO;
+using Chirp.Infrastructure;
+using Chirp.Infrastructure.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages;
+
+public class AboutMe(IAuthorRepository authorRepository, ICheepRepository cheepRepository) : PageModel
+{
+    public AuthorDTO Author { get; set; }
+    public List<CheepDTO> Cheeps { get; set; }
+    public async void OnGet()
+    {
+        Author = await authorRepository.GetAuthorByName(User.Identity.Name!);
+        Cheeps = (await cheepRepository.GetCheepsFromAuthor(Author.Name)).ToList();
+    }
+}
