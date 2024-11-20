db45397af0e28e9e4e5ce083d637843e4c90a565
Christoffer Grünberg <gryn@itu.dk>
2024-11-20 15:57:57 +0100

    Implemented tests for Follow

--- Chirp/test/RepositoryTests/AuthorRepositoryUnitTest.cs
+++ Chirp/test/RepositoryTests/AuthorRepositoryUnitTest.cs
@@ -122,8 +122,10 @@ public class AuthorRepositoryUnitTest(InMemoryDBFixture<ChirpDBContext> fixture)
 
         #endregion
 
+        //Act
         var result = await authorRepo.GetAuthorFollows(author.UserName!);
-
+        
+        //Assert
         Assert.NotNull(result);
         Assert.NotEmpty(result);
         Assert.Single(result);
@@ -137,26 +139,98 @@ public class AuthorRepositoryUnitTest(InMemoryDBFixture<ChirpDBContext> fixture)
     [Fact]
     public async Task Follow_UserToFollowDoesNotExist_ThrowsException()
     {
+        //Arrange
+        var context = fixture.GetContext();
+        var authorRepo = new AuthorRepository(context);
+        
+        var author = TestUtils.CreateTestAuthor("mr. test");
+        context.Authors.Add(author);
+        await context.SaveChangesAsync();
+        
+        //Act & Assert
+        await Assert.ThrowsAsync<UserDoesNotExist>(() => authorRepo.Follow(author.UserName!, "mr. follow"));
     }
 
     [Fact]
     public async Task Follow_UserDoesNotExist_ThrowsException()
     {
+        //Arrange
+        var context = fixture.GetContext();
+        var authorRepo = new AuthorRepository(context);
+        
+        var author = TestUtils.CreateTestAuthor("mr. follow");
+        context.Authors.Add(author);
+        await context.SaveChangesAsync();
+        
+        //Act & Assert
+        await Assert.ThrowsAsync<UserDoesNotExist>(() => authorRepo.Follow("mr. test", author.UserName!));
     }
 
     [Fact]
     public async Task Follow_UserToFollowIsItSelf_ThrowsException()
     {
+        //Arrange
+        var context = fixture.GetContext();
+        var authorRepo = new AuthorRepository(context);
+        
+        var author = TestUtils.CreateTestAuthor("mr. test");
+        context.Authors.Add(author);
+        await context.SaveChangesAsync();
+        
+        //Act & Assert
+        await Assert.ThrowsAsync<ArgumentException>(() => authorRepo.Follow(author.UserName!, author.UserName!));
     }
 
     [Fact]
     public async Task Follow_UserToFollowAlreadyFollowed_ReturnsFalse()
     {
+        //Arrange
+        var context = fixture.GetContext();
+        var authorRepo = new AuthorRepository(context);
+        
+        var author = TestUtils.CreateTestAuthor("mr. test");
+        var following = TestUtils.CreateTestAuthor("mr. follow");
+        author.Follows.Add(following);
+        
+        context.Authors.Add(following);
+        context.Authors.Add(author);
+        
+        await context.SaveChangesAsync();
+        
+        //Act
+        var result = await authorRepo.Follow(author.UserName!, following.UserName!);
+        
+        //Assert
+        Assert.False(result);
     }
 
     [Fact]
     public async Task Follow_UserToFollowIsNotFollowed_ReturnsTrue()
     {
+        //Arrange
+        var context = fixture.GetContext();
+        var authorRepo = new AuthorRepository(context);
+        
+        var author = TestUtils.CreateTestAuthor("mr. test");
+        var following = TestUtils.CreateTestAuthor("mr. follow");
+        
+        context.Authors.Add(following);
+        context.Authors.Add(author);
+        
+        await context.SaveChangesAsync();
+        
+        //Act
+        var result = await authorRepo.Follow(author.UserName!, following.UserName!);
+        var authorsFollowing = context.Authors
+            .Where(a => a.NormalizedUserName == author.NormalizedUserName)
+            .SelectMany(a => a.Follows).ToList();
+        
+        //Assert
+        Assert.True(result);
+        Assert.NotEmpty(authorsFollowing);
+        Assert.Single(authorsFollowing);
+        Assert.Equal(following.UserName, authorsFollowing[0].UserName);
+        
     }
 
     #endregion
