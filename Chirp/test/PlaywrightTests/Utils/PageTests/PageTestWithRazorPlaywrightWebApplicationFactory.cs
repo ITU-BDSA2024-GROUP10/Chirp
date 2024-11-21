09867847d9040a1b0dd170f940617a309f34d1b2
stoftot <gryn@itu.dk>
2024-11-21 13:54:07 +0100

    Created method to generate cheeps

--- Chirp/test/PlaywrightTests/Utils/PageTests/PageTestWithRazorPlaywrightWebApplicationFactory.cs
+++ Chirp/test/PlaywrightTests/Utils/PageTests/PageTestWithRazorPlaywrightWebApplicationFactory.cs
@@ -1,3 +1,5 @@
+using Chirp.Infrastructure;
+using Chirp.Infrastructure.Model;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using PlaywrightTests.Utils.Factories;
@@ -7,8 +9,9 @@ namespace PlaywrightTests.Utils.PageTests;
 public class PageTestWithRazorPlaywrightWebApplicationFactory : PageTest
 {
     protected const string RazorBaseUrl = "http://localhost:5273";
-    protected RazorPlaywrightWebApplicationFactory razorFactory;
+    protected RazorPlaywrightWebApplicationFactory RazorFactory;
     private HttpClient _razorClient;
+    protected PageUtils RazorPageUtils;
 
     public override BrowserNewContextOptions ContextOptions()
     {
@@ -21,13 +24,14 @@ public class PageTestWithRazorPlaywrightWebApplicationFactory : PageTest
     }
 
     [OneTimeSetUp]
-    public void RazorOneTimeSetUp() => razorFactory = new RazorPlaywrightWebApplicationFactory(RazorBaseUrl);
+    public void RazorOneTimeSetUp() => RazorFactory = new RazorPlaywrightWebApplicationFactory(RazorBaseUrl);
 
     [SetUp]
     public void RazorSetup()
     {
-        _razorClient = razorFactory.CreateClient();
-        razorFactory.ResetDB();
+        _razorClient = RazorFactory.CreateClient();
+        RazorFactory.ResetDB();
+        RazorPageUtils = new PageUtils(Page);
     }
     
     [TearDown]
@@ -37,6 +41,38 @@ public class PageTestWithRazorPlaywrightWebApplicationFactory : PageTest
     public async Task RazorOneTimeTearDown()
     {
         _razorClient.Dispose();
-        await razorFactory.DisposeAsync();
+        await RazorFactory.DisposeAsync();
+    }
+
+
+    protected async Task<Cheep> GenerateCheep(Author author, string message = "test")
+    {
+        return (await GenerateCheeps(author, 1, message)).First();
+    }
+    
+    protected async Task<IEnumerable<Cheep>> GenerateCheeps(Author author, int count, string message = "test")
+    {
+        var context = RazorFactory.GetDbContext();
+        // This is to make sure the context now the user exists, since we add user through the user manager.
+        // Otherwise, it will try to add the user again, and it will throw an exception.
+        if (context.Authors.Local.All(a => a.Id != author.Id))
+        {
+            context.Authors.Attach(author);
+        }
+        
+        var cheeps = new List<Cheep>();
+        for (var i = 0; i < count; i++)
+        {
+            cheeps.Add(new Cheep()
+            {
+                Author = author,
+                Message = message,
+                TimeStamp = DateTime.Now.AddHours(i),
+            });
+        }
+        
+        await context.Cheeps.AddRangeAsync(cheeps);
+        await context.SaveChangesAsync();
+        return cheeps;
     }
 }
