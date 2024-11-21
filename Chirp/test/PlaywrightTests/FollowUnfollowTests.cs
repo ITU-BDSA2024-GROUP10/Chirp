b26d08beb9be0392fcc524b0ceb73aa02381db17
stoftot <gryn@itu.dk>
2024-11-21 13:55:46 +0100

    Updated FollowUnfollow tests to use new utils

--- Chirp/test/PlaywrightTests/FollowUnfollowTests.cs
+++ Chirp/test/PlaywrightTests/FollowUnfollowTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.Playwright;
+using NUnit.Framework.Internal;
 using PlaywrightTests.Utils;
 using PlaywrightTests.Utils.PageTests;
 
@@ -8,51 +9,9 @@ namespace PlaywrightTests;
 [NonParallelizable]
 public class FollowUnfollowTests : PageTestWithRazorPlaywrightWebApplicationFactory
 {
-    private const string DefaultPassword = "Password123!";
-
-    // helper method: register a user
-    private async Task RegisterUser(string userName, string email, string password)
-    {
-        await Page.GotoAsync("/");
-        await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
-        await Page.GetByPlaceholder("name", new() { Exact = true }).ClickAsync();
-        await Page.GetByPlaceholder("name", new() { Exact = true }).FillAsync(userName);
-        await Page.GetByPlaceholder("name@example.com").ClickAsync();
-        await Page.GetByPlaceholder("name@example.com").FillAsync(email);
-        await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
-        await Page.GetByLabel("Password", new() { Exact = true }).FillAsync(password);
-        await Page.GetByLabel("Confirm Password").ClickAsync();
-        await Page.GetByLabel("Confirm Password").FillAsync(password);
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your account" }).ClickAsync();
-    }
-
-    // helper method: log in as a user
-    private async Task LoginAsUser(string userName, string password)
-    {
-        await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
-        await Page.GetByPlaceholder("Username").ClickAsync();
-        await Page.GetByPlaceholder("Username").FillAsync(userName);
-        await Page.GetByPlaceholder("password").ClickAsync();
-        await Page.GetByPlaceholder("password").FillAsync(password);
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-    }
-
-    // helper method for log out
-    private async Task Logout()
-    {
-        await Page.GetByRole(AriaRole.Link, new() { Name = "logout" }).ClickAsync();
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Click here to Logout" }).ClickAsync();
-    }
-
-    // helper method for write a cheep
-    private async Task WriteCheep(string message)
-    {
-        await Page.Locator("#Message").ClickAsync();
-        await Page.Locator("#Message").FillAsync(message);
-        await Page.GetByRole(AriaRole.Button, new() { Name = "Share" }).ClickAsync();
-    }
-
+    private TestAuthor _testAuthor;
+    private TestAuthor _testFollower;
+    
     // helper method for following author
     private async Task FollowAuthor(string authorCheepText)
     {
@@ -80,18 +39,20 @@ public class FollowUnfollowTests : PageTestWithRazorPlaywrightWebApplicationFact
     [SetUp]
     public async Task SetUp()
     {
-        await RegisterUser("author", "author@itu.dk", DefaultPassword);
-        await LoginAsUser("author", DefaultPassword);
-        await WriteCheep("test");
-        await Logout();
-
-        await RegisterUser("follower", "follower@itu.dk", DefaultPassword);
-        await LoginAsUser("follower", DefaultPassword);
+        _testAuthor = new TestAuthorBuilder(RazorFactory.GetUserManager())
+            .WithUsernameAndEmail("author")
+            .Create();
+        await GenerateCheep(_testAuthor.author);
+        
+        _testFollower = new TestAuthorBuilder(RazorFactory.GetUserManager())
+            .WithUsernameAndEmail("follower")
+            .Create();
     }
 
     [Test]
     public async Task UserCanFollowAuthor()
     {
+        await RazorPageUtils.Login(_testFollower);
         await GoToPublicTimeline();
         await FollowAuthor("author follow test");
         await GoToPrivateTimeline();
@@ -101,6 +62,8 @@ public class FollowUnfollowTests : PageTestWithRazorPlaywrightWebApplicationFact
     [Test]
     public async Task UserCanUnfollowAuthor()
     {
+        await RazorPageUtils.Login(_testFollower);
+        
         await GoToPublicTimeline();
         await FollowAuthor("author follow test");
         await GoToPrivateTimeline();
@@ -111,8 +74,7 @@ public class FollowUnfollowTests : PageTestWithRazorPlaywrightWebApplicationFact
     [Test]
     public async Task UserCannotFollowSelf()
     {
-        await Logout();
-        await LoginAsUser("author", DefaultPassword);
+        await RazorPageUtils.Login(_testAuthor);
         var followButton = Page.Locator("li").Filter(new() { HasText = "author follow test" }).GetByRole(AriaRole.Button);
         await Expect(followButton).ToBeHiddenAsync();
     }
@@ -120,12 +82,12 @@ public class FollowUnfollowTests : PageTestWithRazorPlaywrightWebApplicationFact
     [Test]
     public async Task CanOnlySeeOwnAndFollowedCheeps()
     {
-        await Logout();
-        await RegisterUser("author2", "author2@itu.dk", DefaultPassword);
-        await LoginAsUser("author2", DefaultPassword);
-        await WriteCheep("this is author2's cheep");
-        await Logout();
-        await LoginAsUser("follower", DefaultPassword);
+        var testAuthor2 = new TestAuthorBuilder(RazorFactory.GetUserManager())
+            .WithUsernameAndEmail("author2")
+            .Create();
+        await GenerateCheep(testAuthor2.author, "this is author2's cheep");
+        await RazorPageUtils.Login(_testFollower);
+        
         await GoToPublicTimeline();
         await FollowAuthor("author follow test");
         await GoToPrivateTimeline();
@@ -136,20 +98,22 @@ public class FollowUnfollowTests : PageTestWithRazorPlaywrightWebApplicationFact
     [Test]
     public async Task UserStaysOnPageAfterUnfollowOnPrivateTimeline()
     {
+        await RazorPageUtils.Login(_testFollower);
         await GoToPublicTimeline();
         await FollowAuthor("author follow test");
         await GoToPrivateTimeline();
         await UnfollowAuthor("author unfollow test");
-        await Expect(Page.GetByText("There are no cheeps so far.")).ToBeVisibleAsync();
+        Assert.That(Page.Url, Is.EqualTo($"{RazorBaseUrl}/{_testFollower.UserName!.ToLower()}?page=1"));
     }
 
     [Test]
     public async Task UserStaysOnPageAfterFollowAndUnfollowOnPublicTimeline()
     {
+        await RazorPageUtils.Login(_testFollower);
         await GoToPublicTimeline();
         await FollowAuthor("author follow test");
+        Assert.That(Page.Url, Is.EqualTo($"{RazorBaseUrl}/?page=1"));
         await UnfollowAuthor("author unfollow test");
-        await Expect(Page.Locator("li").Filter(new() { HasText = "author follow test" })).ToBeVisibleAsync();
-        await Expect(Page.GetByText("There are no cheeps so far.")).ToBeHiddenAsync();
+        Assert.That(Page.Url, Is.EqualTo($"{RazorBaseUrl}/?page=1"));
     }
 }
