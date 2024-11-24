fec79c6c1c621aec5123fa1e4c587cb56bf86c1f
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-11-24 18:57:29 +0100

    Changed to use Services instead of Repository
    Co-Authored-By: Mathias <62756914+math045b@users.noreply.github.com>

--- Chirp/src/Chirp.Web/Pages/AboutMe.cshtml.cs
+++ Chirp/src/Chirp.Web/Pages/AboutMe.cshtml.cs
@@ -9,14 +9,14 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Chirp.Web.Pages;
 
-public class AboutMe(IAuthorRepository authorRepository, ICheepRepository cheepRepository) : PageModel
+public class AboutMe(IAuthorService authorService, ICheepService cheepService, SignInManager<Author> signInManager, UserManager<Author?> userManager) : PageModel
 {
-    public Author Author { get; set; }
+    public Author? Author { get; set; }
     public List<CheepDTO> Cheeps { get; set; } = [];
     public async void OnGet()
     {
-        Cheeps = (await cheepRepository.GetCheepsFromAuthor(Author!.Name)).ToList();
         Author = await userManager.FindByNameAsync(User.Identity!.Name!);
+        Cheeps = (cheepService.GetCheepsFromAuthor(Author!.UserName!)).ToList();
     }
 
     public ActionResult OnPost()
