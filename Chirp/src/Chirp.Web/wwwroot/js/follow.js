b1b6d4441e89a8fb90abfbedf5d6aafecee16bed
Alex Tilgreen Mogensen <alext@itu.dk>
2024-11-25 19:05:03 +0100

    moved script to seperate file

---
+++ Chirp/src/Chirp.Web/wwwroot/js/follow.js
@@ -0,0 +1,14 @@
+async function toggleFollow(button, followName, shouldFollow) {
+    const response = await fetch('/follow', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `followName=${followName}&shouldFollow=${shouldFollow}&returnUrl=${window.location.pathname}`
+    });
+
+    if (response.ok) {
+        button.innerText = shouldFollow ? 'unfollow' : 'follow';
+        button.setAttribute('onclick', `toggleFollow(this, '${followName}', ${!shouldFollow})`);
+    } else {
+        alert('Failed to update follow status');
+    }
+}
