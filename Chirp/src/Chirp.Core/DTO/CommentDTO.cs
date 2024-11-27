033ffd50ecb84adec5bfe7f3ed31a2f5ebfe80ca
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-11-27 13:08:25 +0100

    Implemented CommentDTO

--- Chirp/src/Chirp.Core/DTO/CommentDTO.cs
+++ Chirp/src/Chirp.Core/DTO/CommentDTO.cs
@@ -1,6 +1,9 @@
 namespace Chirp.Core.DTO;
 
-public class CommentDTO
+public class CommentDTO (string author,  int cheepId, string message, long unixTimestamp)
 {
-    
+    public string Author { get; set; } = author;
+    public int CheepId { get; set; } = cheepId;
+    public string Message { get; set; } = message;
+    public long UnixTimestamp { get; set; } = unixTimestamp;
 }
