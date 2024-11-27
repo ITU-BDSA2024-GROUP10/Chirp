9567294c83e644d3df7ed93f28c1e9c626a64957
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-11-27 13:07:56 +0100

    Implemented Comment datamodel

--- Chirp/src/Chirp.Infrastructure/Model/Comment.cs
+++ Chirp/src/Chirp.Infrastructure/Model/Comment.cs
@@ -2,4 +2,23 @@ namespace Chirp.Infrastructure.Model;
 
 public class Comment
 {
+    public int Id { get; set; }
+    public required Author Author { get; set; }
+    public required Cheep Cheep { get; set; }
+    public DateTime TimeStamp { get; set; } 
+    public required string Message { get; set; }
+
+    public Comment()
+    {
+        
+    }
+
+    public Comment(int id, Author author, Cheep cheep, string content, DateTime timeStamp)
+    {
+        Id = id;
+        Author = author;
+        Cheep = cheep;
+        Message = content;
+        TimeStamp = timeStamp;
+    }
 }
