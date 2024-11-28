cf622e10513774f7c88b64a8c442010114952b14
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-11-28 15:30:11 +0100

    Added commenttest class

---
+++ Chirp/test/PlaywrightTests/UITests/CommentTests.cs
@@ -0,0 +1,11 @@
+using Duende.IdentityServer.Extensions;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Playwright;
+using NUnit.Framework.Internal;
+using PlaywrightTests.Utils;
+using PlaywrightTests.Utils.PageTests;
+namespace PlaywrightTests.UITests;
+
+public class CommentTests :  PageTestWithRazorPlaywrightWebApplicationFactory
+{
+}
