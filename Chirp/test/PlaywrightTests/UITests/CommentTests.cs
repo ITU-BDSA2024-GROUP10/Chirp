85c87c7d4887a35a1c6bfab26fbc42c646c306c1
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2024-11-28 15:30:41 +0100

    Added tests

--- Chirp/test/PlaywrightTests/UITests/CommentTests.cs
+++ Chirp/test/PlaywrightTests/UITests/CommentTests.cs
@@ -19,4 +19,39 @@ public class CommentTests :  PageTestWithRazorPlaywrightWebApplicationFactory
         await GenerateCheep(_testAuthor.author);
 
     }
+    
+    [Test]
+    public async Task SpecificCheepViewExists()
+    {
+        await GenerateCheep(_testAuthor.author, "this is author");
+        await Page.GotoAsync("/");
+        await Page.Locator("#messagelist div").Filter(new() { HasText = "—" }).GetByRole(AriaRole.Link).First.ClickAsync();
+        await Expect( Page.GetByRole(AriaRole.Link, new() { Name = "author" })).ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task CommentCountUpdates()
+    {
+        await GenerateCheep(_testAuthor.author, "this is author");
+        await RazorPageUtils.Login(_testAuthor);
+        await Page.GotoAsync("/");
+        await Expect(Page.Locator("li").Filter(new() { HasText = "author" }).Locator("small").Nth(1))
+            .ToHaveTextAsync("0");
+        await Page.Locator("#messagelist div").Filter(new() { HasText = "—" }).GetByRole(AriaRole.Link).First.ClickAsync();
+        await Page.GetByPlaceholder("Write a comment").ClickAsync();
+        await Page.GetByPlaceholder("Write a comment").FillAsync("This is a comment");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Post" }).ClickAsync();
+        await Page.GetByText("This is a comment").ClickAsync();
+        await Expect(Page.GetByText("1", new() { Exact = true })).ToHaveTextAsync("1");
+    }
+
+    [Test]
+    public async Task CannotPostCommentsWhenNotLoggedIn()
+    {
+        await GenerateCheep(_testAuthor.author, "this is author");
+        await Page.GotoAsync("/");
+        await Page.Locator("#messagelist div").Filter(new() { HasText = "—" }).GetByRole(AriaRole.Link).First.ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Log in to comment on this" }))
+            .ToBeVisibleAsync();
+    }
 }
