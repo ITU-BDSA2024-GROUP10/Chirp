779c795081c4e3dc8e2d7b5866011940bdae83be
anthoncastillo <145575420+anthoncastillo@users.noreply.github.com>
2024-11-29 16:11:18 +0100

    Added ProfileImage for an AuthorDTO

--- Chirp/src/Chirp.Core/DTO/AuthorDTO.cs
+++ Chirp/src/Chirp.Core/DTO/AuthorDTO.cs
@@ -4,4 +4,5 @@ public class AuthorDTO(string name, string email)
 {
     public string Name { get; set; } = name;
     public string Email { get; set; } = email;
+    public byte[]? ProfileImage { get; set; } = ProfileImage;
 }
