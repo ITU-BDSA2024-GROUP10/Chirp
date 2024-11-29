24454431c364419f3e70ffa2ff7b7d30b292a8aa
anthoncastillo <145575420+anthoncastillo@users.noreply.github.com>
2024-11-29 16:58:27 +0100

    Added ImageMap to ViewComponent

--- Chirp/src/Chirp.Web/ViewComponents/CheepListViewComponent.cs
+++ Chirp/src/Chirp.Web/ViewComponents/CheepListViewComponent.cs
@@ -5,10 +5,11 @@ namespace Chirp.Web.ViewComponents;
 
 public class CheepListViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke(IEnumerable<CheepDTO> cheeps, String targetPage)
+    public IViewComponentResult Invoke(IEnumerable<CheepDTO> cheeps, Dictionary<string, byte[]> imageMap, String targetPage)
     {
         ViewBag.TargetPage = targetPage;
         ViewBag.Cheeps = cheeps;
+        ViewBag.ImageMap = imageMap;
         return View("Default");
     }
 }
