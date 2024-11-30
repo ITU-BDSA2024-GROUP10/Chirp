8dbc3ff7095ca86ba4a359b8a127655e18c6d24f
stoftot <gryn@itu.dk>
2024-11-30 22:57:29 +0100

    Updated view component to include videos

--- Chirp/src/Chirp.Web/ViewComponents/CheepListViewComponent.cs
+++ Chirp/src/Chirp.Web/ViewComponents/CheepListViewComponent.cs
@@ -5,10 +5,27 @@ namespace Chirp.Web.ViewComponents;
 
 public class CheepListViewComponent : ViewComponent
 {
-    public IViewComponentResult Invoke(IEnumerable<CheepDTO> cheeps, String targetPage)
+    private readonly List<string> _videoNames =
+    [
+        "~/videos/Kittens.mp4",
+        "~/videos/SubwaySurfers_1.mp4",
+        "~/videos/SubwaySurfers_2.mp4",
+        "~/videos/TempleRun.mp4"
+    ];
+    
+    public IViewComponentResult Invoke(IEnumerable<CheepDTO> cheeps, string targetPage)
     {
         ViewBag.TargetPage = targetPage;
         ViewBag.Cheeps = cheeps;
+        ViewBag.videoListLeft = GetVideos().Select(v =>Url.Content(v)).ToList();
+        ViewBag.videoListRight = GetVideos().Select(v =>Url.Content(v)).ToList();
+        
         return View("Default");
+    }
+
+    public List<string> GetVideos()
+    {
+        var random = new Random();
+        return _videoNames.OrderBy(x => random.Next()).ToList();
     }
 }
