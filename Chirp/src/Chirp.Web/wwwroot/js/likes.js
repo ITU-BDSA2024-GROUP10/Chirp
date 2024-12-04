f75aa995640573e9f879ab4d7126b4bed41cddfc
Mathias <mlao@itu.dk>
2024-12-04 14:35:05 +0100

    Added styling to buttons
    Co-Authored-By: Alex <79690147+AlexTMIT@users.noreply.github.com>

--- Chirp/src/Chirp.Web/wwwroot/js/likes.js
+++ Chirp/src/Chirp.Web/wwwroot/js/likes.js
@@ -1,5 +1,6 @@
 async function toggleLike(button, cheepId, isLiking) {
     const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    const likeCountElement= document.getElementById(`likecount-${cheepId}`);
 
     try {
         const response = await fetch('/like?handler=ToggleLike', {
@@ -16,36 +17,9 @@ async function toggleLike(button, cheepId, isLiking) {
             const likeCount = data.likeCount;
             isLiking = isLiking === 'true' || isLiking === true;
             const newIsLiking = !isLiking;
-            button.innerText = `${newIsLiking ? 'Like' : 'Unlike'} (${likeCount})`;
             button.setAttribute('onclick', `toggleLike(this, ${cheepId}, ${newIsLiking})`);
-        } else {
-            alert('Failed to update like status');
-        }
-    } catch (error) {
-        console.error('Failed to toggle like:', error);
-    }
-}
-
-async function toggleLikeHeart(cheepId, isLiking) {
-    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-    try {
-        const response = await fetch('/like?handler=ToggleLike', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'RequestVerificationToken': csrfToken
-            },
-            body: `cheepId=${encodeURIComponent(cheepId)}&isLiking=${isLiking}`
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            const likeCount = data.likeCount;
-            isLiking = isLiking === 'true' || isLiking === true;
-            const newIsLiking = !isLiking;
-            //button.innerText = `${newIsLiking ? 'Like' : 'Unlike'} (${likeCount})`;
-            //button.setAttribute('onclick', `toggleLike(this, ${cheepId}, ${newIsLiking})`);
+            button.setAttribute('class', `like-button${isLiking ? "-liked" : ""}`);
+            likeCountElement.textContent = likeCount;
         } else {
             alert('Failed to update like status');
         }
