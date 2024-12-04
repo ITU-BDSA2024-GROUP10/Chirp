5280f29411665b921279458ad4ad5e5d21ff3427
Mathias <mlao@itu.dk>
2024-12-04 11:15:32 +0100

    Added reduntant button
    I need someone else to fix this

--- Chirp/src/Chirp.Web/wwwroot/js/likes.js
+++ Chirp/src/Chirp.Web/wwwroot/js/likes.js
@@ -25,3 +25,31 @@ async function toggleLike(button, cheepId, isLiking) {
         console.error('Failed to toggle like:', error);
     }
 }
+
+async function toggleLikeHeart(cheepId, isLiking) {
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+    try {
+        const response = await fetch('/like?handler=ToggleLike', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'RequestVerificationToken': csrfToken
+            },
+            body: `cheepId=${encodeURIComponent(cheepId)}&isLiking=${isLiking}`
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            const likeCount = data.likeCount;
+            isLiking = isLiking === 'true' || isLiking === true;
+            const newIsLiking = !isLiking;
+            //button.innerText = `${newIsLiking ? 'Like' : 'Unlike'} (${likeCount})`;
+            //button.setAttribute('onclick', `toggleLike(this, ${cheepId}, ${newIsLiking})`);
+        } else {
+            alert('Failed to update like status');
+        }
+    } catch (error) {
+        console.error('Failed to toggle like:', error);
+    }
+}
